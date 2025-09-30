320bd578d1cd84bb84e1f6b4c3cc84e255adac5b
D2rkL1fe <d2rkl1fe@gmail.com>
2025-09-30 22:44:54 +0300

    Player Controller

---
+++ entities/player/player.gd
@@ -0,0 +1,15 @@
+class_name Player extends CharacterBody2D
+
+@export var sprite : AnimatedSprite2D
+
+var speed : float = 160.0
+
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction:
+		sprite.flip_h = direction.x < 0
+	
+	velocity = lerp(velocity, speed * direction, 16.0 * delta)
+	
+	move_and_slide()
