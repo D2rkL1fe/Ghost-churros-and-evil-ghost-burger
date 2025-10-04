71b594c0a58abc59de3db53218c61d133d66c816
DPH888 <dimitarhadjiev2007@gmail.com>
2025-10-04 23:53:34 +0300

    Added churros counter

---
+++ scenes/UI.gd
@@ -0,0 +1,8 @@
+extends CanvasLayer
+@export var label : Label
+#
+func _ready():
+	GlobalStats.call_Curros.connect(churrosChanged);
+func churrosChanged():
+	label.text="Churros: "+str(GlobalStats.churros_counter);
+	set("theme_override_colors/font_color", Color("Red"))
