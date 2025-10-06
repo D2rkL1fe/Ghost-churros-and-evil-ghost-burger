5e8d74b4f2a56f660bcfb7b13d3f3dae69ec2f04
D2rkL1fe <d2rkl1fe@gmail.com>
2025-10-06 23:21:01 +0300

    Refactoring UI

---
+++ ui/settings/settings.gd
@@ -0,0 +1,22 @@
+extends Control
+
+
+func _on_slider_sfx_value_changed(value: float) -> void:
+	pass # Replace with function body.
+
+func _on_slider_music_value_changed(value: float) -> void:
+	pass # Replace with function body.
+
+
+func _on_check_sfx_toggled(toggled_on: bool) -> void:
+	AudioServer.set_bus_mute(1, !toggled_on)
+
+func _on_check_music_toggled(toggled_on: bool) -> void:
+	pass # Replace with function body.
+
+
+func _on_restart_pressed() -> void:
+	get_tree().reload_current_scene()
+
+func _on_menu_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menu/menu.tscn")
