f2101e12bc24f2ec52713abd16a6da910dce3d42
D2rkL1fe <d2rkl1fe@gmail.com>
2025-10-18 16:35:15 +0300

    Clean up

--- scenes/splash/splash.gd
+++ scenes/splash/splash.gd
@@ -1,13 +1,10 @@
 extends Control
 
 func transition():
-	print(123)
 	Global.transition("res://scenes/menu/menu.tscn")
 
 func whoosh():
-	print(321)
 	SoundPlayer.play_sound(SoundPlayer.WHOOSH)
 
 func whiip():
-	print(444)
 	SoundPlayer.play_sound(SoundPlayer.WHIIP)
